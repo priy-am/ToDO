78fcbf032febc9920d2e42806b095a264e394c58
priy-am <priyam2101753@gmail.com>
2024-12-05 00:55:05 +0530

    update2.0

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -1,6 +1,6 @@
 function Popup({ handleDeleteTrue, handleDeleteFalse }) {
   return (
-    <div className="w-96 bg-[#bcd5f3a8] py-3 px-4 rounded-2xl fixed top-[40%] left-[40%] ">
+    <div className="w-96 bg-[#bcd5f3a8] py-3 px-4 rounded-2xl fixed top-[40%] md:left-[40%]  left-2 ">
       <div className=" bg-[#3b4b547d] py-2 rounded-xl">
         <p className="text-center mb-4">You sure you wanna delete?</p>
         <div className="flex gap-4 items-center justify-center mb-2">
